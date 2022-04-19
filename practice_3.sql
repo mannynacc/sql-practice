98709b6f743a8c0006ed740bb4fcb33b4f3d4f00
Manuel Cota <56351434+mannynacc@users.noreply.github.com>
2022-04-19 17:50:49 -0500

    Finished practice 3

---
+++ practice_3.sql
@@ -0,0 +1,47 @@
+-- Basic addition, subtraction and multiplication with SQL
+
+SELECT 2 + 2;    -- addition
+SELECT 9 - 1;    -- subtraction
+SELECT 3 * 4;    -- multiplication
+
+
+-- Division with SQL
+
+SELECT 11 / 6;   -- integer division
+SELECT 11 % 6;   -- modulo division
+SELECT 11.0 / 6; -- decimal division
+SELECT CAST(11 AS numeric(3,1)) / 6;
+
+-- Exponents, roots and factorials
+
+SELECT 3 ^ 4;         -- exponentiation
+SELECT |/ 10;         -- square root (operator)
+SELECT sqrt(10);      -- square root (function)
+SELECT ||/ 10;        -- cube root
+SELECT factorial(4);  -- factorial (function)
+SELECT 4 !;           -- factorial (operator; PostgreSQL 13 and earlier only)
+
+-- SQL percentile functions
+
+CREATE TABLE percentile_test (
+    numbers integer
+);
+
+INSERT INTO percentile_test (numbers) VALUES
+    (1), (2), (3), (4), (5), (6);
+
+SELECT
+    percentile_cont(.5)
+    WITHIN GROUP (ORDER BY numbers),
+    percentile_disc(.5)
+    WITHIN GROUP (ORDER BY numbers)
+FROM percentile_test;
+
+
+-- Using sum(), avg(), and percentile_cont() functions
+
+SELECT sum(pop_est_2019) AS county_sum,
+       round(avg(pop_est_2019), 0) AS county_average,
+       percentile_cont(.5)
+       WITHIN GROUP (ORDER BY pop_est_2019) AS county_median
+FROM us_counties_pop_est_2019;
